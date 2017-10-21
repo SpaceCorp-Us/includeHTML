a889ae4977cb6f62f49b50abe7e825b87bd466eb
Karl Saenz <spacecorp.us@gmail.com>
2017-10-20 21:47:54 -0700

    prep'd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  	title = 'Angular JS';
+	//
+	items: any[] = [];
+	//
+	constructor() {
+		this.addRecords(5);
+	}
+	//
+	addRecords(number: number) {
+		for(var i = 1; i <= number; i++){
+			this.items.push({
+				id: i,
+				text: "Story " + i
+			});
+		}
+	}
+	//
 }
