ba92ea2bf93b12a01a34a98ed23a3bf64567a28c
Karl Saenz <spacecorp.us@gmail.com>
2017-10-21 18:04:58 -0700

    ready for upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { APP_ROUTING } from './app.routing';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }    from '@angular/http';
 import { NgModule } from '@angular/core';
 import { SplashComponent } from './splash/splash.component';
 import { ViewerComponent } from './viewer/viewer.component';
@@ -12,7 +13,8 @@ import { ViewerComponent } from './viewer/viewer.component';
     SplashComponent
   ],
   imports: [
-    BrowserModule,
+	 BrowserModule,
+	 HttpModule,
     APP_ROUTING
   ],
   providers: [],
--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,5 +1,13 @@
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import 'rxjs/Rx';
+
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Headers, Http, RequestOptions, Response, ResponseType } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-viewer',
@@ -7,17 +15,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent implements OnInit {
+	//
+	private myHeaders = new Headers({'Content-Type':'text/HTML'});
 	itemID: string;
+	//
+	@Input()
+	htmlContent: any = '<h3>TESTING</h3>';
+	//
   	constructor(
 		private activeRoute: ActivatedRoute,
-		private router: Router
-  	) {}
-
-  	ngOnInit() {
-		this.activeRoute.params.subscribe( (params: Params) => {
-			this.itemID = params['id'];
-			// console.log(this.itemID);
-		});
+		// private responseType: ResponseType,
+		private router: Router,
+		private http: Http
+	) {}
+	//
+	private handleError (error: Response | any) {
+		console.error('ViewerComponent::handleError', error);
+		return Observable.throw(error);
   	}
-
+	//
+  	ngOnInit() {
+		this.activeRoute.params
+			.subscribe( (params: Params) => {
+				this.itemID = params['id'];
+				this.fetchHTML('../data/story-'+ this.itemID +'.html');
+				console.log(this.htmlContent); // <<<<<<<<<<<< Problem, still not getting data !
+				// console.log(this.itemID);
+			})
+		;
+	}
+	// Get html content to pass to Component
+	fetchHTML(pathFile: string) {
+		let options = new RequestOptions({ headers: this.myHeaders });
+		console.log(pathFile);
+		this.http.get(pathFile, options)
+			.map( (html: any) => {
+				this.htmlContent = html.data;
+			})
+			.catch(this.handleError)
+		;
+	}
+	//
 }
